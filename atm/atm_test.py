9fbbf88d79fdaafbc8fd121b5de18fe69f2f0d62
El Ouassouli Abdelaali <abdelaalielouassouli@icloud.com>
2018-02-10 16:27:22 +0000

    make atm test compatible with the new atm file

--- atm/atm_test.py
+++ atm/atm_test.py
@@ -1,9 +1,14 @@
-import atm
+from atm import Atm
 
-print 'amount', atm.amount
-atm.atm_func(3)
-atm.atm_func(277)
-print 'amount', atm.amount
-atm.atm_func(200)
-print 'amount', atm.amount
-atm.atm_func(300)
+b1 = 500
+b2 = 1000
+
+a1 = Atm(b1, "Smart Bank")
+a2 = Atm(b2, "Baraka Bank")
+
+a1.atm_balance = a1.withdraw(500)
+a2.atm_balance = a2.withdraw(200)
+
+a1.atm_balance = a1.withdraw(500)
+a2.atm_balance = a2.withdraw(900)
+
